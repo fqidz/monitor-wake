7496dbcc1adce825acfbf51d813761d4a00d7db5
fqidz <faidz.arante@gmail.com>
2025-09-12 22:02:00 +0300

    feat: init

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC = clang
+ARGS = -xc\
+	   -std=c99\
+	   -ggdb3\
+	   -Weverything\
+	   -Werror\
+	   -Wno-used-but-marked-unused\
+	   -Wno-padded\
+	   -Wno-declaration-after-statement\
+	   -Wno-covered-switch-default\
+	   -Wno-unsafe-buffer-usage\
+	   -Wno-missing-prototypes\
+	   -Wno-disabled-macro-expansion
+
+monitor-wake: monitor-wake.c
+	$(CC) $(ARGS) `pkg-config --cflags --libs dbus-1` monitor-wake.c -o monitor-wake
+
+clean:
+	rm monitor-wake
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.dbus ];
+
+          packages = [
+            # pkgs.clang-tools should come before pkgs.clang or else clangd can't detect headers
+            # https://github.com/NixOS/nixpkgs/issues/76486
+            pkgs.clang-tools
+            pkgs.clang
+            pkgs.gnumake
+            pkgs.gdb
+
+            # manpaths dont appear in devshells
+            # https://github.com/NixOS/nixpkgs/pull/234367
+            # workaround here:
+            # https://discourse.nixos.org/t/how-to-get-postgres-man-pages-in-a-devshell/47321/2?u=fqidz
+            (pkgs.buildEnv {
+              name = "devShell";
+              paths = [
+                pkgs.man-pages-posix
+                pkgs.man-pages
+                pkgs.clang-manpages
+              ];
+            })
+          ];
+
+          # C_INCLUDE_PATH = pkgs.lib.makeLibraryPath [
+          #   pkgs.dbus.lib
+          #   pkgs.dbus.dev
+          # ];
+        };
+      }
+    );
+}
